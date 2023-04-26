88cc90d22eeac4fd37c821f1cd7e0f394fad3a43
Monu-Chaudhary <chaudharymonu45@gmail.com>
2023-04-26 13:44:37 -0400

    used data stored in local memory to perform post operatins

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,5 +1,23 @@
 import express from 'express';
 
+let articles = [
+    {
+        name: 'learn-react',
+        votes: 0,
+        comments: [],
+    },
+    {
+        name: 'learn-mongo',
+        votes: 0,
+        comments: [],
+    },
+    {
+        name: 'learn-node',
+        votes: 0,
+        comments: [],
+    },
+]
+
 const app = express(); // create a new express app
 app.use(express.json());
 
@@ -7,13 +25,29 @@ app.get('/hello', (req, res) => {
     res.send("Hello! How do you do?");
 });
 
-app.post('/hello', (req, res) => {
-    res.send(`hello ${req.body.name}`);
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params
+    const article = articles.find(article => name == article.name)
+    if (article) {
+        article.votes++
+        res.send(`The new vote count for ${name} is ${article.votes}`);
+    }
+    else {
+        res.send('No such article found.')
+    }
 });
 
-app.post('/hello/:name', (req, res) => {
-    let {name} = req.params;
-    res.send(`hello ${name}`);
+app.put('/api/articles/:name/comment', (req, res) => {
+    const {postedBy, comment} = req.body
+    const { name } = req.params
+    const article = articles.find(article => name == article.name)
+    if (article) {
+        article.comments.push({'posedBy': postedBy, 'comment': comment})
+        res.send(article.comments);
+    }
+    else {
+        res.send('No such article found.')
+    }
 });
 
 app.listen(8000, () => {
