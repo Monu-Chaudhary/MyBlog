d9d910265a3a9cfa96b05771bc82356011f344df
Monu-Chaudhary <chaudharymonu45@gmail.com>
2023-04-26 12:51:05 -0400

    working with postman, request body and params

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,9 +1,19 @@
 import express from 'express';
 
-const app = express() // create a new express app
+const app = express(); // create a new express app
+app.use(express.json());
 
 app.get('/hello', (req, res) => {
     res.send("Hello! How do you do?");
+});
+
+app.post('/hello', (req, res) => {
+    res.send(`hello ${req.body.name}`);
+});
+
+app.post('/hello/:name', (req, res) => {
+    let {name} = req.params;
+    res.send(`hello ${name}`);
 });
 
 app.listen(8000, () => {
